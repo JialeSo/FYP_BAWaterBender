630bd1167d1150f98f3d094da2adfb19a4682f0d
Natalie <103411864+puppymatrix@users.noreply.github.com>
2025-09-02 14:53:04 +0800

    priority mapping script

--- etl/priority_mapping.py
+++ etl/priority_mapping.py
@@ -1,57 +1,78 @@
+
 import pandas as pd
 
-#  Load ARC GIS amenities dataset 
-df = pd.read_csv('data/amenities.csv')
-print(df.head())
-print(df.shape)
+##load data
+df = pd.read_csv('arcgis/amenities.csv')
+df.shape
+df.head()
 
-# check unique amenity types 
+#check unique amenity types, these will be amenity subcategories
 unique_amenity_types = df['amenity_type'].unique()
 print(unique_amenity_types)
 print(len(unique_amenity_types))
+print(df['source_file'].count())
+print(df.shape)
 
-## load unique amenities and give it index 
-## added id just in case we want it
-amenitiesDB = pd.DataFrame(unique_amenity_types, columns=['amenity_type'])
-amenitiesDB["prioritisation_amenity_id"] = amenitiesDB.index+1
+# map amenity_type as subcategories to categories
+subcat_to_cat_mapping = {
+    "emergency_services": ['fire_services'],
+    "healthcare_facilities": [],
+    "essential_services": [ 'childcare_clean', 'post_offices' , 'police' ],
+    "residential": ['hdb_buildings'],
+    "education_institutions": ['preschools', 'special_education' ,'moe_schools', 'higher_education', 'kindergartens' ],
+    "transport_services": ['bus_depots','bus_interchanges_terminals','bus_stops', 'mrt_station_exits' ] ,
+    "tourism": ['tourist_attractions', 'hotels', 'historic_sites' ],
+    "community_spaces":['synagogues', 'sports_centres', 'stadiums' ,'swimming_complex' , 'churches', 'community_clubs', 'concert_halls', 'mosques', 'libraries', 'chinese_temples' , 'sikh_temples', 'indian_temples' , 'parkfacilities'],
+    "government_services": ['courts'],
+    "retail_services": ['hdb_points_shp'],
+    "others": ['other_institutions']}
 
-## decide on how to prioritise each amenity
-### higher priority, lower number (i.e. 1 --> critical, 5 --> low priority)
-### categorised based on importance to community (e.g. emergency services, essential services)
-
-priority_mapping = {
-    1: [
-        "fire_services", "police", "courts",
-        "mrt_station_exits", "bus_interchanges_terminals",
-        "bus_depots", "bus_stops"
-    ],
-    2: [
-        "moe_schools", "higher_education", "special_education",
-        "childcare_clean", "kindergartens", "preschools",
-        "community_clubs", "post_offices", "libraries",
-        "hdb_buildings", "hdb_points_shp"
-    ],
-    3: [
-        "mosques", "churches", "chinese_temples", "indian_temples",
-        "sikh_temples", "synagogues", "concert_halls", "historic_sites"
-    ],
-    4: [
-        "stadiums", "sports_centres", "swimming_complex",
-        "parkfacilities", "tourist_attractions", "hotels"
-    ]
+# decide on categories and (priority, weight)
+# PRIORITY: lower number, more important --> based on importance to community 
+## decided based on scale of 1 to 5, with 1 being critical to community and 5 being low impact on community if no access
+# WEIGHT: higher number, more weight --> based on assumptinos made on usage/footfall 
+## decided based on scale of 1 to 5, with 1 being low usage/footfall (used by avg person <1x a week) and 5 being high footfall (used by avg person 5-7x a week) with some manual override execptions (e.g. emergency services should always be available)
+category_priority_weight = {
+    "emergency_services": [1, 5],
+    "healthcare_facilities": [1, 5],
+    "essential_services": [1, 4],
+    "residential": [1, 5],
+    "education_institutions": [2, 4],
+    "transport_services": [2, 4],
+    "tourism": [5, 1],
+    "community_spaces": [4, 2],
+    "government_services": [3, 3],
+    "financial_services": [3, 2],
+    "retail_services": [4, 2],
+    "recreation": [5, 2]
 }
 
-priority_labels = {1: "Critical", 2: "High", 3: "Medium", 4: "Low"}
+# given df with priority and weight, calculate importance score and add label
+def amenity_importance(df, score_col="importance_score", score_label="importance_label", bins=[1, 2, 3, 5,8, 25], labels=["Negligible", "Low", "Moderate", "High", "Critical"]):
+    # calculate importance score
+    df[score_col] =  (df["amenity_weight"])**2/df["amenity_priority"]
+    # add importance labels
+    df[score_label] = pd.cut(df[score_col], bins=bins, labels=labels)
 
-## map back to amenitiesDB 
-amenitiesDB["priority"] = amenitiesDB["amenity_type"].map(
-    {v: k for k, vs in priority_mapping.items() for v in vs}
-)
+    return df
+
+# create categoryDB
+category_data = [(category, values[0], values[1]) for category, values in category_priority_weight.items()]
+categoryDB = pd.DataFrame(category_data, columns=["amenity_category", "amenity_priority", "amenity_weight"])
+print(categoryDB.head())
+
+# create subcat_to_catDB
+subcat_to_catDB = pd.DataFrame([(subcat, cat) for cat, subcats in subcat_to_cat_mapping.items() for subcat in subcats], columns=["amenity_type", "amenity_category"])
+print(subcat_to_catDB.head())
+
+
+# label ARCGIS data with category information
+df = df.merge(subcat_to_catDB, on="amenity_type", how="left")
 
 ## add priority labels
-# amenitiesDB["priority_label"] = amenitiesDB["priority"].map(priority_labels)
+df = df.merge(categoryDB, on="amenity_category", how="left")
+print(df.head())
 
-## get new db with priority
-amenities_prioritised = df.merge(amenitiesDB, on="amenity_type", how="left")
-amenities_prioritised.shape
-amenities_prioritised.head()
+## calculate importance score and add label
+df = amenity_importance(df)
+print(df.head())
