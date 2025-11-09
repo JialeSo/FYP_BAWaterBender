62404504c1981998f9842d73b96f685d01a55ad2
JialeSo <148540758+JialeSo@users.noreply.github.com>
2025-11-09 10:44:06 +0800

    updatedMain.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "@/components/ui/sonner";
 import App from "./App";
 import './index.css';
-import { MapDataProvider } from "./context/MapDataContext.jsx";
+import { MapDataProvider } from "./context/mapDataContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <BrowserRouter>
